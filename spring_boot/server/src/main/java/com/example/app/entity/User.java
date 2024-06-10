13ea28c08f22bab3f774523a730a0e19e8338ff5
takuNishi0201 <donndonndonkiti@yahoo.co.jp>
2024-06-10 14:10:17 +0900

    create springBoot

---
+++ spring_boot/server/src/main/java/com/example/app/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  protected User() {}
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("{id:%d,name:%s}", id, name);
+  }
+}
